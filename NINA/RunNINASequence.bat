23bb07a3a5113cbd5beaa6d2d8d499248fdd41ac
Nick Ambrose <nick.a.ambrose@gmail.com>
2025-09-19 10:31:16 -0500

    2025-09-19 Darks and 10 Mosaic Panel 19. Used TS for the first time

--- NINA/RunNINASequence.bat
+++ NINA/RunNINASequence.bat
@@ -1,2 +1,10 @@
 @echo off
-"C:\Program Files\N.I.N.A. - Nighttime Imaging 'N' Astronomy\NINA.EXE" --sequencefile "C:\Users\nicka\Astro\NINA\NINA_Sequences\CurrentSequence.json" --runsequence
+
+@echo off
+REM set applicationPath="C:\Users\PrimaLuceLab\AppData\Local\iOptronCommander2017\iOptron Commander 2017.exe"
+
+REM Launch the application with the specified options
+REM start "" %applicationPath% %options%
+
+"C:\Program Files\N.I.N.A. - Nighttime Imaging 'N' Astronomy\NINA.EXE" --sequencefile "C:\Users\nicka\Astro\NINA\NINA_Sequences\TargetScheduler.json" --runsequence
+
